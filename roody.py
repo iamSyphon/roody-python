db81945bdc5a2eab86bdb24f7830d2de02d5459a
garb <garb@steam.cat>
2016-10-22 20:46:12 +1100

    Added invite link.

--- roody.py
+++ roody.py
@@ -26,5 +26,8 @@ async def on_message(message):
     if client.user.mention in message.content:
         await client.send_message(message.channel, '{0}, {1}'.format(message.author.mention, get_insult()))
 
+    if "roody-inv" in message.content:
+        await client.send_message(message.author, 'What the fuck cunt? Trying to add me to your shitty server. Well here is the link cunt: https://discordapp.com/oauth2/authorize?client_id=238261917796401152&scope=bot')
+
 
 client.run('token')
